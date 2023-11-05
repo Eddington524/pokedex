26e95e581f8eb3ec6e203d91462861da9ce69594
eddington524 <iamsam8687@gmail.com>
2023-11-05 23:50:37 +0900

    add: getPokemonDetail,getSpecies, getEvolutionData

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,9 +2,32 @@ import axios from 'axios';
 
 export const POKE_API = 'https://pokeapi.co/api/v2/pokemon';
 
-export const getPokemonData = async (signal: AbortSignal, offset: number) => {
-	const response = await axios.get(POKE_API, {
+const axiosInstance = axios.create({
+	baseURL: 'https://pokeapi.co/api/v2',
+});
+
+export const getPokemonListData = async (
+	signal: AbortSignal,
+	offset: number
+) => {
+	const response = await axiosInstance.get('/pokemon', {
 		params: { limit: 20, offset: offset },
 	});
 	return response.data;
+};
+
+export const getPokemonDetailData = async (pokemonName: string) => {
+	const response = await axiosInstance.get(`/pokemon/${pokemonName}`);
+	return response.data;
+};
+
+export const getSpeciesData = async (speciesNum: string) => {
+	const response = await axiosInstance.get(`/pokemon-species/${speciesNum}`);
+
+	return response.data.evolution_chain.url;
+};
+
+export const getEvolutionData = async (evChainNum: string) => {
+	const response = await axiosInstance.get(`/evolution-chain/${evChainNum}`);
+	return response.data.chain;
 };
